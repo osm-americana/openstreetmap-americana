8c11680a3bb939ad9f160c6b64696759c3747eef
Brian Sperlongano <zelonewolf@gmail.com>
2021-07-09 07:32:35 -0400

    Use simplified array push syntax

--- style/layers.js
+++ style/layers.js
@@ -3,24 +3,26 @@
 */
 var americanaLayers = [];
 
-americanaLayers.push(layerBackground);
+americanaLayers.push(
+  layerBackground,
 
-americanaLayers.push(layerWater);
+  layerWater,
 
-americanaLayers.push(layerTunnelMotorwayCasing);
-americanaLayers.push(layerTunnelMotorwayLinkCasing);
-americanaLayers.push(layerTunnelMotorway);
-americanaLayers.push(layerTunnelMotorwayLink);
-americanaLayers.push(layerTunnelOneway);
-americanaLayers.push(layerTunnelOnewayLink);
+  layerTunnelMotorwayCasing,
+  laerrTunnelMotorwayLinkCasing,
+  layerTunnelMotorway,
+  layerTunnelMotorwayLink,
+  layerTunnelOneway,
+  layerTunnelOnewayLink,
 
-americanaLayers.push(layerMotorwayCasing);
+  layerMotorwayCasing,
 
-americanaLayers.push(layerMotorwayLinkCasing);
-americanaLayers.push(layerMotorway);
-americanaLayers.push(layerMotorwayLink);
-americanaLayers.push(layerRoadOneway);
-americanaLayers.push(layerRoadOnewayLink);
+  layerMotorwayLinkCasing,
+  layerMotorway,
+  layerMotorwayLink,
+  layerRoadOneway,
+  layerRoadOnewayLink
+);
 
 //One layer at a time to handle stacked bridges
 for (let i = 1; i <= 5; i++) {
@@ -34,14 +36,16 @@ for (let i = 1; i <= 5; i++) {
   ].forEach((layer) => americanaLayers.push(restrictLayer(layer, i)));
 }
 
-americanaLayers.push(layerMotorwayLabel);
+americanaLayers.push(
+  layerMotorwayLabel,
 
-americanaLayers.push(layerHighwayShieldInterstate);
+  layerHighwayShieldInterstate,
 
-americanaLayers.push(layerPlaceCity);
-americanaLayers.push(layerPlaceState);
-americanaLayers.push(layerPlaceCountryOther);
-americanaLayers.push(layerPlaceCountry3);
-americanaLayers.push(layerPlaceCountry2);
-americanaLayers.push(layerPlaceCountry1);
-americanaLayers.push(layerPlaceContinent);
+  layerPlaceCity,
+  layerPlaceState,
+  layerPlaceCountryOther,
+  layerPlaceCountry3,
+  layerPlaceCountry2,
+  layerPlaceCountry1,
+  layerPlaceContinent
+);
