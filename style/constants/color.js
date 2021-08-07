9fb28fe5674292892af83b90eeffb47054e77e19
Brian Sperlongano <zelonewolf@gmail.com>
2021-08-06 22:18:57 -0400

    Remove road constants from the color file

--- style/constants/color.js
+++ style/constants/color.js
@@ -1,68 +1,6 @@
-var minzoomBrunnel = 11;
-var clrTransparent = "hsla(0, 0%, 0%, 0)";
-
 var clrWaterFill = "hsl(211, 42%, 70%)";
 var clrWaterLine = "hsl(211, 73%, 78%)";
 var clrWaterIntermittent = "hsl(205, 89%, 83%)";
-
-var hueMoto = 354;
-var hueTrunk = 0;
-
-var clrMotoLowZoom = [
-  "interpolate",
-  ["exponential", 1.2],
-  ["zoom"],
-  4,
-  `hsl(${hueMoto}, 70%, 76%)`,
-  6,
-  `hsl(${hueMoto}, 70%, 66%)`,
-  minzoomBrunnel - 0.5,
-  `hsl(${hueMoto}, 70%, 60%)`,
-];
-var clrMoto = clrMotoLowZoom.concat(14, `hsl(${hueMoto}, 71%, 45%)`);
-
-var clrTrunk = `hsl(${hueTrunk}, 70%, 28%)`;
-
-var clrMotoCaseLowZoom = [
-  "interpolate",
-  ["exponential", 1.2],
-  ["zoom"],
-  4,
-  `hsl(${hueMoto}, 10%, 85%)`,
-  6,
-  `hsl(${hueMoto}, 60%, 50%)`,
-  minzoomBrunnel - 0.5,
-  `hsl(${hueMoto}, 71%, 40%)`,
-];
-var clrMotoCase = clrMotoCaseLowZoom.concat(14, `hsl(${hueMoto}, 71%, 23%)`);
-
-var clrTrunkCase = `hsl(${hueTrunk}, 70%, 18%)`;
-
-var tunDashArray = [
-  "step",
-  ["zoom"],
-  ["literal", [1]],
-  minzoomBrunnel,
-  ["literal", [0.5, 0.25]],
-];
-
-var clrMotoBridgeCase = clrMotoCaseLowZoom.concat(
-  minzoomBrunnel + 0.5,
-  `hsl(${hueMoto}, 71%, 10%)`
-);
-var clrMotoTun = clrMotoLowZoom.concat(
-  minzoomBrunnel + 0.5,
-  `hsl(${hueMoto}, 71%, 90%)`
-);
-var clrTrunkTun = `hsl(${hueTrunk}, 41%, 90%)`;
-
-var clrMotoTunCase = clrMotoCaseLowZoom.concat(
-  minzoomBrunnel + 0.5,
-  `hsl(${hueMoto}, 71%, 75%)`
-);
-var clrTrunkTunCase = clrTrunkCase;
-
-var clrBridgeCase = "black";
 
 var hueBorder = 0;
 var hueBorderCase = 281;
