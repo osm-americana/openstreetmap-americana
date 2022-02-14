8d7b994265eb1f51ed1e04dcd89d48522fb0f6a5
Brian Sperlongano <zelonewolf@gmail.com>
2022-02-13 22:33:32 -0500

    Add photon search

---
+++ style/search.js
@@ -0,0 +1,137 @@
+"use strict";
+
+let searchInput = document.getElementById("geocoder-search-input");
+
+searchInput.addEventListener("input", search);
+searchInput.addEventListener("keydown", arrowNavigate);
+
+var liveResults = document.getElementById("geocoder-live-results");
+var divMap = document.getElementById("map");
+var resultSelectIndex = -1;
+var resultGeometry = [];
+var resultPoint = [];
+
+function collapseArray(arr, delimiter) {
+  return arr
+    .filter(function (x) {
+      return x !== undefined && x != "";
+    })
+    .join(delimiter);
+}
+
+function geocoderResultEntry(result) {
+  let p = result.properties;
+
+  let addr = collapseArray([p.housenumber, p.street], " ");
+  let name = p.name;
+
+  if (name == undefined) {
+    name = addr;
+    addr = undefined;
+  }
+
+  let description = collapseArray(
+    [addr, p.city, p.county, p.state, p.country],
+    ", "
+  );
+
+  return `
+      <div class="gc-result-item">
+        <div class="gc-result-category">${p.type}</div>
+        <div class="gc-result-name">${name}</div>
+        <div class="gc-result-description">${description}</div>
+      </div>
+    `;
+}
+
+function geocoderResponse(data) {
+  liveResults.innerHTML = "";
+
+  for (var i = 0; i < data.features.length; i++) {
+    resultGeometry[i] = data.features[i].properties.extent;
+    resultPoint[i] = data.features[i].geometry.coordinates;
+    console.log(resultGeometry[i]);
+    liveResults.innerHTML += geocoderResultEntry(data.features[i]);
+  }
+}
+
+function search(e) {
+  let queryTerm = e.target.value;
+  resultSelectIndex = -1;
+  if (queryTerm.length < 3) {
+    liveResults.innerHTML = "";
+    return;
+  }
+
+  let position = map.getCenter();
+  let searchQuery =
+    "https://photon.komoot.io/api/?q=" +
+    encodeURIComponent(e.target.value) +
+    "&lat=" +
+    position.lat +
+    "&lon=" +
+    position.lng +
+    "&limit=3";
+
+  fetch(searchQuery)
+    .then((response) => response.json())
+    .then((data) => geocoderResponse(data));
+}
+
+function arrowNavigate(e) {
+  e = e || window.event;
+
+  let priorIndex = resultSelectIndex;
+
+  switch (e.keyCode) {
+    case 38:
+      // up arrow
+      resultSelectIndex--;
+      break;
+    case 40:
+      // down arrow
+      resultSelectIndex++;
+      break;
+    case 13:
+      let navIndex = resultSelectIndex < 0 ? 0 : resultSelectIndex;
+      let bbox = resultGeometry[navIndex];
+      let center = resultPoint[navIndex];
+      resultSelectIndex = -1;
+      resultGeometry = [];
+      resultPoint = [];
+      liveResults.innerHTML = "";
+
+      //Clear search box
+      searchInput.value = "";
+      searchInput.blur();
+
+      console.log(bbox);
+      console.log(center);
+
+      //Zoom map to search result
+      if (bbox != undefined) {
+        map.fitBounds(bbox);
+      } else {
+        map.setCenter({ lat: center[1], lon: center[0] });
+      }
+
+      return;
+    default:
+      return;
+  }
+
+  if (resultSelectIndex >= liveResults.children.length) {
+    resultSelectIndex = liveResults.children.length - 1;
+  }
+  if (resultSelectIndex < -1) {
+    resultSelectIndex = -1;
+  }
+
+  if (priorIndex >= 0) {
+    liveResults.children.item(priorIndex).className = "gc-result-item";
+  }
+  if (resultSelectIndex >= 0) {
+    liveResults.children.item(resultSelectIndex).className =
+      "gc-result-item-selected";
+  }
+}
