7760c72d5164f620b0531c673020a12e11a236aa
Minh Nguyễn <mxn@1ec5.org>
2022-02-19 16:34:19 -0800

    Ohio county road shields (#174)
    * Added Ohio county road shields
    
    * Removed metadata from Ohio shields
    
    * Templated some strings

--- style/js/shield_defs.js
+++ style/js/shield_defs.js
@@ -351,7 +351,7 @@ export function loadShields(shieldImages) {
     "Hudson",
     "Salem",
     "Gloucester",
-  ].forEach((county) => (shields["US:NJ:" + county] = usMUTCDCountyShield));
+  ].forEach((county) => (shields[`US:NJ:${county}`] = usMUTCDCountyShield));
 
   shields["US:NY"] = {
     backgroundImage: shieldImages.shield40_us_ny,
@@ -375,6 +375,81 @@ export function loadShields(shieldImages) {
       shieldImages.shield40_us_oh_3,
     ],
     norefImage: shieldImages.shield40_us_oh_turnpike,
+    textColor: "black",
+    padding: {
+      left: 3,
+      right: 3,
+      top: 4,
+      bottom: 6,
+    },
+  };
+
+  ["COL", "JEF", "MAH", "OTT", "SEN", "STA", "SUM", "TUS"].forEach(
+    // Yellow on blue pentagon
+    (county) => (shields[`US:OH:${county}`] = usMUTCDCountyShield)
+  );
+  [
+    "ATH",
+    "BEL",
+    "GUE",
+    "HAR",
+    "HEN",
+    "MOE",
+    "PAU",
+    "WAS",
+    "WIL",
+    "WYA",
+  ].forEach(
+    // White on green rectangle
+    (county) =>
+      (shields[`US:OH:${county}`] = roundedRectShield(
+        "#006747",
+        "white",
+        "white",
+        2,
+        1,
+        null
+      ))
+  );
+  ["MED", "NOB"].forEach(
+    // White on blue rectangle
+    (county) =>
+      (shields[`US:OH:${county}`] = roundedRectShield(
+        "#003f87",
+        "white",
+        "white",
+        2,
+        1,
+        null
+      ))
+  );
+  ["TRU", "VIN"].forEach(
+    // Black on yellow rectangle
+    (county) =>
+      (shields[`US:OH:${county}`] = roundedRectShield(
+        "#ffcd00",
+        "black",
+        "black",
+        2,
+        1,
+        null
+      ))
+  );
+  shields["US:OH:ASD"] = {
+    backgroundImage: [shieldImages.shield40_us_oh_asd],
+    textColor: "#006747",
+    padding: {
+      left: 6,
+      right: 3,
+      top: 4,
+      bottom: 7,
+    },
+  };
+  shields["US:OH:SCI"] = {
+    backgroundImage: [
+      shieldImages.shield40_us_oh_sci_2,
+      shieldImages.shield40_us_oh_sci_3,
+    ],
     textColor: "black",
     padding: {
       left: 3,
