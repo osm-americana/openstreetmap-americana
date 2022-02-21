f882610ff4ce0c4178205ee87851d54be2802003
Brian Sperlongano <zelonewolf@gmail.com>
2022-02-20 22:15:56 -0500

    Add AWS config

---
+++ style/configs/config.aws.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*
+Planetiler tile server, hosted by StreetFerret
+*/
+const TILESOURCE = [
+  "https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/data/v3/{z}/{x}/{y}.pbf",
+];
+
+export default {
+  TILESOURCE,
+};
