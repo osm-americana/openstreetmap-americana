6bcd0cef97dbab4e5bf027bdf3dd142dc8065e60
Brian Sperlongano <zelonewolf@gmail.com>
2022-02-20 21:37:12 -0500

    Remove duplicated GitHub banner

--- style/configs/config.maptiler.js
+++ style/configs/config.maptiler.js
@@ -12,22 +12,10 @@ Go to Account->Keys and create a key to paste here:
 const MAPTILER_KEY = "<your MapTiler key>";
 
 const OPENMAPTILES_URL = `https://api.maptiler.com/tiles/v3-openmaptiles/tiles.json?key=${MAPTILER_KEY}`;
-const ATTRIBUTION_LOGO = `<a id="attribution-logo" href="https://www.maptiler.com/">
-<img
-  src="https://api.maptiler.com/resources/logo.svg"
-  alt="MapTiler logo"
-/></a>
-<a href="https://github.com/ZeLonewolf/openstreetmap-americana/"
-><img
-  loading="lazy"
-  width="149"
-  height="149"
-  src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149"
-  class="attachment-full size-full"
-  alt="Fork me on GitHub"
-  data-recalc-dims="1"
-  style="position: absolute; top: 0; right: 0; border: 0; z-index: 100"
-/></a>`;
+const ATTRIBUTION_LOGO = `
+<a id="attribution-logo" href="https://www.maptiler.com/">
+  <img src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/>
+</a>`;
 const ATTRIBUTION_TEXT =
   '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>';
 
