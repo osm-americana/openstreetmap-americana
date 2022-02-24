ef552143b1dfc87b1697b98b7fdd5d2eac4cf4b3
Minh Nguyễn <mxn@1ec5.org>
2022-02-24 02:52:47 -0800

    Added route shields for the Philippines

--- style/js/shield_defs.js
+++ style/js/shield_defs.js
@@ -798,6 +798,34 @@ export function loadShields(shieldImages) {
     },
   };
 
+  shields["PH:N"] =
+    shields["PH:N:primary"] =
+    shields["PH:N:secondary"] =
+    shields["ph:nhn"] =
+      {
+        backgroundImage: [
+          shieldImages.shield40_ph_national_2,
+          shieldImages.shield40_ph_national_3,
+        ],
+        textColor: "black",
+        padding: {
+          left: 2,
+          right: 2,
+          top: 2,
+          bottom: 6,
+        },
+      };
+  shields["PH:E"] = {
+    backgroundImage: shieldImages.shield40_ph_expressway_2,
+    textColor: "black",
+    padding: {
+      left: 2,
+      right: 2,
+      top: 2,
+      bottom: 6,
+    },
+  };
+
   shields["TW:freeway"] = {
     backgroundImage: shieldImages.shield40_tw_freeway,
     textLayoutConstraint: ShieldText.ellipseTextConstraint,
