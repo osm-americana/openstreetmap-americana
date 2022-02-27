10003146a5544db33c125feb0f49767a42d3c6d4
Brian Sperlongano <zelonewolf@gmail.com>
2022-02-27 18:33:26 -0500

    Remove convertPathData svgo optimization

--- style/configs/config.svgo.cjs
+++ style/configs/config.svgo.cjs
@@ -6,4 +6,15 @@ module.exports = {
     indent: 1,
     pretty: true,
   },
+  plugins: [
+    {
+      name: "preset-default",
+      params: {
+        overrides: {
+          // disable plugins that cause problems with spritezero
+          convertPathData: false,
+        },
+      },
+    },
+  ],
 };
