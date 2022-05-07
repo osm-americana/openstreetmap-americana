74939640f9201123ceb8fd37d8266b0c25bd99f1
Minh Nguyễn <mxn@1ec5.org>
2022-05-07 12:37:49 -0700

    Removed non-namespaced Croatian networks

--- style/js/shield_defs.js
+++ style/js/shield_defs.js
@@ -2066,10 +2066,6 @@ export function loadShields(shieldImages) {
     },
   };
 
-  // Croatia
-  shields["Autoceste"] = shields["SI:AC"];
-  shields["Državne ceste"] = shields["cz:national"];
-
   // Netherlands
   shields["NL:A"] = roundedRectShield(
     "#ba1e10",
