c1da1fc400c0749342a7a91fb129c19c71774a91
AnonymousAlligatorOSM <101657206+AnonymousAlligatorOSM@users.noreply.github.com>
2022-05-14 17:13:34 -0500

    airports: Tweak color
    Co-authored-by: Zeke Farwell <zekefarwell@users.noreply.github.com>

--- style/constants/color.js
+++ style/constants/color.js
@@ -13,7 +13,7 @@ export const parkFill = "hsl(136, 41%, 89%)";
 export const parkOutline = "hsl(136, 41%, 79%)";
 export const parkLabel = "hsl(136, 71%, 29%)";
 
-export const airportFill = "hsla(292, 41%, 89%, .3)";
-export const airportOutline = "hsl(292, 41%, 79%)";
-export const airportRunway = "hsl(270, 41%, 79%)";
-export const airportLabel = "hsl(270, 71%, 29%)";
+export const airportFill = "hsl(250, 41%, 95%)";
+export const airportOutline = "hsl(250, 41%, 79%)";
+export const airportRunway = "hsl(250, 41%, 79%)";
+export const airportLabel = "hsl(250, 71%, 29%)";
