f1b135b1b51b5b049ef42965eecb740de0bcc3c1
Josh Lee <jleedev@gmail.com>
2022-06-13 13:43:46 +0000

    Additional error handling

--- scripts/serve.js
+++ scripts/serve.js
@@ -2,5 +2,7 @@ import serve from "create-serve";
 
 import { build } from "./build.js";
 
-await build(() => serve.update(), { minify: false });
+await build(() => serve.update(), { minify: false }).catch(() =>
+  process.exit(1)
+);
 serve.start({ port: 1776, root: "dist/" });
