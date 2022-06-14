7012441d015dd7e799a515a7fbfee97d8482d18c
Josh Lee <jleedev@gmail.com>
2022-06-13 20:33:49 -0400

    Add opening in web browser

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,8 +1,12 @@
 import serve from "create-serve";
+import open from "open";
 
 import { build } from "./build.js";
+
+const port = 1776;
 
 await build(() => serve.update(), { minify: false }).catch(() =>
   process.exit(1)
 );
-serve.start({ port: 1776, root: "dist/" });
+serve.start({ port, root: "dist/" });
+open(`http://localhost:${port}/`);
