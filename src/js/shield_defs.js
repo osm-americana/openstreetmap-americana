c4b7c7e00c07bc492dd931cb56ac15dc9711821d
riiga <riiga_92@hotmail.com>
2022-06-16 21:39:04 +0200

    Reorder to sort by country code

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -2616,6 +2616,16 @@ export function loadShields(shieldImages) {
     Color.shields.white
   );
 
+  // Åland Islands
+  shields["AX:main"] = roundedRectShield(
+    Color.shields.red,
+    Color.shields.white
+  );
+  shields["AX:province"] = roundedRectShield(
+    Color.shields.blue,
+    Color.shields.white
+  );
+
   // Bosnia and Herzegovina
   shields["ba:Autoceste"] = roundedRectShield(
     Color.shields.green,
@@ -2867,28 +2877,6 @@ export function loadShields(shieldImages) {
     Color.shields.white
   );
 
-  // Slovenia
-  shields["SI:AC"] = wideHexagonGreenShield;
-
-  // Slovakia
-  shields["sk:national"] = roundedRectShield(
-    Color.shields.red,
-    Color.shields.white,
-    Color.shields.white,
-    2,
-    1,
-    35
-  );
-
-  // Ukraine
-  shields["ua:international"] = roundedRectShield(
-    Color.shields.blue,
-    Color.shields.white
-  );
-
-  // Kosovo
-  shields["XK:motorway"] = wideHexagonGreenShield;
-
   // Sweden
   [
     "RV", // Riksväg - national road
@@ -2935,15 +2923,27 @@ export function loadShields(shieldImages) {
     null
   );
 
-  // Åland Islands
-  shields["AX:main"] = roundedRectShield(
+  // Slovenia
+  shields["SI:AC"] = wideHexagonGreenShield;
+
+  // Slovakia
+  shields["sk:national"] = roundedRectShield(
     Color.shields.red,
-    Color.shields.white
+    Color.shields.white,
+    Color.shields.white,
+    2,
+    1,
+    35
   );
-  shields["AX:province"] = roundedRectShield(
+
+  // Ukraine
+  shields["ua:international"] = roundedRectShield(
     Color.shields.blue,
     Color.shields.white
   );
+
+  // Kosovo
+  shields["XK:motorway"] = wideHexagonGreenShield;
 
   //Ref-specific cases. Additional entries should be documented in CONTRIBUTE.md
 
