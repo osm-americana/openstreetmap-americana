41c28324e9723417e37840c271e67a170fad659a
riiga <riiga_92@hotmail.com>
2022-06-19 19:19:28 +0200

    Initial support for Icelandic shields

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -2761,6 +2761,34 @@ export function loadShields(shieldImages) {
     backgroundImage: shieldImages.shield40_home_down_blue_3,
   };
 
+  // Iceland
+  [
+    "S", // Stofnvegur - Trunk road
+    "Sx", // Stofnvegur um hálendi - Trunk road in highlands
+    "T", // Tengivegur - Connecting road
+    "L", // Landsvegur - Country road
+    "H", // Héraðsvegur - County road
+    // Combinations used as of 2022-06-19
+    // Should these be supported?
+    //"S;H",
+    //"S;Sx",
+    //"S;T",
+    //"S;T;L",
+    //"T;H",
+    //"T;L",
+    //"T;S;L",
+  ].forEach(
+    (icelandic_road_class) =>
+      (shields[`${icelandic_road_class}`] = roundedRectShield(
+        Color.shields.white,
+        Color.shields.black,
+        Color.shields.black,
+        2,
+        1,
+        35
+      ))
+  );
+
   // Italy
   shields["IT:A-road"] = {
     backgroundImage: [
