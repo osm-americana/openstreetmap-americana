3ff1f9c006857d1cf0b36f65cce2dfed179b1655
Minh Nguyễn <mxn@1ec5.org>
2022-11-26 16:10:11 -0800

    Use user-preferred languages in labels

--- src/constants/label.js
+++ src/constants/label.js
@@ -1,9 +1,24 @@
 "use strict";
 
 // Name fields in order of preference
-export const localizedName = [
-  "coalesce",
-  ["get", "name:en"],
-  ["get", "name:latin"],
-  ["get", "name"],
-];
+export const localizedName = (function () {
+  let userLocales =
+    "languages" in navigator ? [...navigator.languages] : [navigator.language];
+  let locales = [];
+  let localeSet = new Set(); // avoid duplicates
+  for (let locale of userLocales) {
+    // Add progressively less specific variants of each user-specified locale.
+    let components = locale.split("-");
+    while (components.length > 0) {
+      let parent = components.join("-");
+      if (!localeSet.has(parent)) locales.push(parent);
+      localeSet.add(parent);
+      components.pop();
+    }
+  }
+  if (locales.at(-1) === "en") {
+    locales.push("latin");
+  }
+  let nameFields = [...locales.map((l) => `name:${l}`), "name"];
+  return ["coalesce", ...nameFields.map((f) => ["get", f])];
+})();
