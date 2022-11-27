93707ad1ff96d1131fe8219de6db4d392a1af434
Brian Sperlongano <zelonewolf@gmail.com>
2022-11-27 15:47:42 -0500

    Add bold italic labels

--- src/layer/water.js
+++ src/layer/water.js
@@ -154,7 +154,7 @@ export const waterPointLabel = {
   filter: ["all", ["==", ["geometry-type"], "Point"]],
   layout: {
     "text-field": Label.localizedName,
-    "text-font": ["OpenHistorical Italic"],
+    "text-font": ["Open Sans Bold Italic"],
     "text-size": [
       "interpolate",
       ["exponential", 2],
