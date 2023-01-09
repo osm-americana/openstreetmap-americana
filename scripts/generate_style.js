3c36f42ccb908162180387887e71f793dc78cb6c
Brian Sperlongano <zelonewolf@gmail.com>
2023-01-08 21:47:42 -0500

    Accept language code as input

--- scripts/generate_style.js
+++ scripts/generate_style.js
@@ -3,10 +3,24 @@
 import * as Style from "../src/js/style.js";
 import config from "../src/config.js";
 
+/**
+ * Accepts a list of languages as parameters
+ *
+ * For example:
+ * generate_style.js en de
+ *
+ * ...will generate a style in English with German fallback
+ */
+
+let languages = process.argv.slice(2);
+if (languages.length == 0) {
+  languages = ["mul"];
+}
+
 let style = Style.build(
   config.OPENMAPTILES_URL,
   "https://zelonewolf.github.io/openstreetmap-americana/sprites/sprite",
-  ["mul"]
+  languages
 );
 
 console.log("%j", style);
