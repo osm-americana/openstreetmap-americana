6237bdbed2ac3d57a908946a42df573b70d03815
Brian Sperlongano <zelonewolf@gmail.com>
2023-01-08 21:07:10 -0500

    Link to config

--- scripts/generate_style.js
+++ scripts/generate_style.js
@@ -1,9 +1,10 @@
 "use strict";
 
 import * as Style from "../src/js/style.js";
+import config from "../src/config.js";
 
 let style = Style.build(
-  "https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/data/v3.json",
+  config.OPENMAPTILES_URL,
   "https://zelonewolf.github.io/openstreetmap-americana/sprites/sprite",
   ["mul"]
 );
