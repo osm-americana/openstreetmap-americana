96596302be9b4c4b9db90cd6f5d8afdcd72e4633
Brian Sperlongano <zelonewolf@gmail.com>
2023-01-14 13:44:32 -0500

    Add low-zoom source

---
+++ src/configs/config.aws-low-zoom.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+Planetiler tile server, hosted at AWS
+*/
+const OPENMAPTILES_URL =
+  "https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/data/planet-low-zoom.json";
+
+/*
+Uncomment this variable to override the shield text halo color. Useful while testing shield design changes.
+Accepts an HTML color name, hex code, or other CSS color value.
+*/
+const SHIELD_TEXT_HALO_COLOR_OVERRIDE = null;
+
+export default {
+  OPENMAPTILES_URL,
+  SHIELD_TEXT_HALO_COLOR_OVERRIDE,
+};
