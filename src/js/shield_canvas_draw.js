028e981f526e7f07f66f15f61aab2225f115ecca
Clay Smalley <claysmalley@gmail.com>
2023-01-16 18:38:08 -0500

    Fix rounded rect shield appearance

--- src/js/shield_canvas_draw.js
+++ src/js/shield_canvas_draw.js
@@ -119,8 +119,8 @@ export function blank(ref) {
 function roundedRectangle(params, ref) {
   let fill = params.fillColor == undefined ? "white" : params.fillColor;
   let outline = params.strokeColor == undefined ? "black" : params.strokeColor;
-  let radius = params.radius == undefined ? 1 : params.radius;
-  let outlineWidth = params.outlineWidth == undefined ? 0 : params.outlineWidth;
+  let radius = params.radius == undefined ? 0 : params.radius;
+  let outlineWidth = params.outlineWidth == undefined ? 1 : params.outlineWidth;
   let rectWidth = params.rectWidth == undefined ? null : params.rectWidth;
 
   if (rectWidth == null) {
