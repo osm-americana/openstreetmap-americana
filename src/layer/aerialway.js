5d4bcb033b639bbebd4aa5c91462a6ee0ee4907b
Clay Smalley <claysmalley@gmail.com>
2023-02-06 13:33:05 -0500

    transition aerialways at z15 from single to double line

--- src/layer/aerialway.js
+++ src/layer/aerialway.js
@@ -5,33 +5,91 @@ import * as Color from "../constants/color.js";
 // Exponent base for inter-zoom interpolation
 let aerialwayExp = 1.2;
 
+let lineWidth = [
+  "interpolate",
+  ["exponential", aerialwayExp],
+  ["zoom"],
+  12,
+  1.2,
+  14.9999,
+  2.4,
+  15,
+  1.2,
+  16,
+  1.2,
+  20,
+  2.4,
+];
+let lineGapWidth = [
+  "interpolate",
+  ["exponential", aerialwayExp],
+  ["zoom"],
+  12,
+  0,
+  15,
+  0,
+  16,
+  1,
+  20,
+  16,
+];
+
+let casingLineWidth = [
+  "interpolate",
+  ["exponential", aerialwayExp],
+  ["zoom"],
+  12,
+  2,
+  15.9999,
+  4,
+  16,
+  2,
+  20,
+  4,
+];
+let casingLineGapWidth = [
+  "interpolate",
+  ["exponential", aerialwayExp],
+  ["zoom"],
+  12,
+  0,
+  16,
+  0,
+  20,
+  14.4,
+];
+
 export const lift = {
   id: "lift",
   type: "line",
   paint: {
     "line-color": Color.aerialwayLine,
-    "line-width": [
-      "interpolate",
-      ["exponential", aerialwayExp],
-      ["zoom"],
-      12,
-      0.6,
-      16,
-      1.2,
-      20,
-      2.4,
+    "line-width": lineWidth,
+    "line-gap-width": lineGapWidth,
+  },
+  filter: [
+    "all",
+    ["==", ["get", "class"], "aerialway"],
+    [
+      "in",
+      ["get", "subclass"],
+      ["literal", ["chair_lift", "cable_car", "gondola", "mixed_lift"]],
     ],
-    "line-gap-width": [
-      "interpolate",
-      ["exponential", aerialwayExp],
-      ["zoom"],
-      12,
-      0.5,
-      16,
-      1,
-      20,
-      16,
-    ],
+  ],
+  layout: {
+    visibility: "visible",
+  },
+  source: "openmaptiles",
+  "source-layer": "transportation",
+};
+
+export const liftCasing = {
+  id: "lift_casing",
+  type: "line",
+  paint: {
+    "line-color": Color.backgroundFill,
+    "line-width": casingLineWidth,
+    "line-gap-width": casingLineGapWidth,
   },
   filter: [
     "all",
@@ -54,18 +112,9 @@ export const dragLift = {
   type: "line",
   paint: {
     "line-color": Color.aerialwayLine,
-    "line-width": [
-      "interpolate",
-      ["exponential", aerialwayExp],
-      ["zoom"],
-      12,
-      0.75,
-      16,
-      1.5,
-      20,
-      3,
-    ],
-    "line-dasharray": [5, 0.5],
+    "line-width": lineWidth,
+    "line-gap-width": lineGapWidth,
+    "line-dasharray": [5, 1],
   },
   filter: [
     "all",
@@ -74,50 +123,6 @@ export const dragLift = {
       "in",
       ["get", "subclass"],
       ["literal", ["drag_lift", "platter", "j-bar", "t-bar"]],
-    ],
-  ],
-  layout: {
-    visibility: "visible",
-  },
-  source: "openmaptiles",
-  "source-layer": "transportation",
-};
-
-export const liftCasing = {
-  id: "lift_casing",
-  type: "line",
-  paint: {
-    "line-color": Color.backgroundFill,
-    "line-width": [
-      "interpolate",
-      ["exponential", aerialwayExp],
-      ["zoom"],
-      12,
-      1,
-      16,
-      2,
-      20,
-      4,
-    ],
-    "line-gap-width": [
-      "interpolate",
-      ["exponential", aerialwayExp],
-      ["zoom"],
-      12,
-      0.01,
-      16,
-      0.01,
-      20,
-      14.4,
-    ],
-  },
-  filter: [
-    "all",
-    ["==", ["get", "class"], "aerialway"],
-    [
-      "in",
-      ["get", "subclass"],
-      ["literal", ["chair_lift", "cable_car", "gondola", "mixed_lift"]],
     ],
   ],
   layout: {
