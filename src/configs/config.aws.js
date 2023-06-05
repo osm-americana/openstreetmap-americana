3f5f068da65e1cd2d06f21ad46e85c4731a0ed23
Brian Sperlongano <zelonewolf@gmail.com>
2023-06-04 20:16:55 -0400

    Switch to ourmap domain

--- src/configs/config.aws.js
+++ src/configs/config.aws.js
@@ -4,7 +4,7 @@
 Planetiler tile server, hosted at AWS
 */
 const OPENMAPTILES_URL =
-  "https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/data/v3.json";
+  "https://tile.ourmap.us/data/v3.json";
 
 /*
 The following two variables override the color of the bounding box and halo of
