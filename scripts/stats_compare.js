51dc895d08fd1a577da9b5f55c1a883e10580b38
Brian Sperlongano <zelonewolf@gmail.com>
2023-07-19 21:59:35 -0400

    Improve readability of stats generator

--- scripts/stats_compare.js
+++ scripts/stats_compare.js
@@ -5,6 +5,15 @@ const stats2 = JSON.parse(process.argv[3]);
 
 const difference = calculateDifference(stats1, stats2);
 
+const diffHeaderRow = [
+  "|           | main          | this PR      | change          | % change        |",
+  "|-----------|--------------:|-------------:|----------------:|----------------:|",
+];
+
+/**
+ * Show comparison of overall aggregate statistics between this PR and previous
+ */
+
 const pctFormat = {
   style: "percent",
   minimumFractionDigits: 1,
@@ -12,66 +21,115 @@ const pctFormat = {
   signDisplay: "exceptZero",
 };
 
+const layersRow = [
+  "Layers",
+  stats1.layerCount,
+  stats2.layerCount,
+  difference.layerCount,
+  ((100 * difference.layerCount) / stats1.layerCount).toLocaleString(
+    "en",
+    pctFormat
+  ),
+];
+
+const sizeRow = [
+  "Size (b)",
+  stats1.styleSize.toLocaleString("en"),
+  stats2.styleSize.toLocaleString("en"),
+  difference.styleSize.toLocaleString("en"),
+  ((100 * difference.styleSize) / stats1.styleSize).toLocaleString(
+    "en",
+    pctFormat
+  ),
+];
+
+const summaryChangeTable = [
+  ...diffHeaderRow,
+  ...layersRow.map((row) => row.join(" | ")),
+  ...sizeRow.map((row) => row.join(" | ")),
+].join("\n");
+
 const summaryChange = `
 
 ## Style size statistics
 
-|           | main          | this PR      | change          | % change        |
-|-----------|--------------:|-------------:|----------------:|----------------:|
-| Layers    | ${stats1.layerCount} | ${stats2.layerCount} | ${
-  difference.layerCount
-} | ${((100 * difference.layerCount) / stats1.layerCount).toLocaleString(
-  undefined,
-  pctFormat
-)} |
-| Size (b)  | ${stats1.styleSize.toLocaleString(
-  "en"
-)} | ${stats2.styleSize.toLocaleString(
-  "en"
-)} | ${difference.styleSize.toLocaleString("en")} | ${(
-  (100 * difference.styleSize) /
-  stats1.styleSize
-).toLocaleString(undefined, pctFormat)}
-
+${summaryChangeTable}
 `;
 
-let layerCountChange = `
+console.log(summaryChange);
+
+/**
+ * Show comparison of the number of layers in each group before and after
+ */
+
+const layerCountChangeRows = [];
+
+for (const layer in difference.layerGroup) {
+  const differenceLayerCount = difference.layerGroup[layer].layerCount;
+  const percentageChange = differenceLayerCount / stats1LayerCount;
+
+  const row = [
+    layer,
+    stats1.layerGroup[layer]?.layerCount || 0,
+    stats2.layerGroup[layer]?.layerCount || 0,
+    differenceLayerCount.toLocaleString("en"),
+    percentageChange.toLocaleString("en", pctFormat),
+  ];
+
+  layerCountChangeRows.push(row);
+}
+
+layerCountChange += layerCountChangeRows.join("\n");
+
+const layerCountChangeTable = [
+  ...diffHeaderRow,
+  ...layerCountChangeRows.map((row) => row.join(" | ")),
+].join("\n");
+
+const layerCountChange = `
 
 ## Layer count comparison
 
-|           | main          | this PR      | change          | % change        |
-|-----------|--------------:|-------------:|----------------:|----------------:|
+${layerCountChangeTable}
 `;
+
+console.log(layerCountChange);
+
+/**
+ * Show comparison of the aggregate size of layers in each group before and after
+ */
+
+const layerSizeChangeRows = [];
 
 for (const layer in difference.layerGroup) {
-  layerCountChange += `${layer} | ${stats1.layerGroup[layer].layerCount} | ${
-    stats2.layerGroup[layer].layerCount
-  } | ${difference.layerGroup[layer].layerCount} | ${(
-    difference.layerGroup[layer].layerCount /
-    stats1.layerGroup[layer].layerCount
-  ).toLocaleString(undefined, pctFormat)}
-`;
+  const stats1LayerSize = stats1.layerGroup[layer]?.size || 0;
+  const stats2LayerSize = stats2.layerGroup[layer]?.size || 0;
+  const differenceLayerSize = difference.layerGroup[layer].size;
+
+  const percentageChange =
+    stats1LayerSize !== 0 ? differenceLayerSize / stats1LayerSize : 0;
+
+  const row = [
+    layer,
+    stats1LayerSize.toLocaleString("en"),
+    stats2LayerSize.toLocaleString("en"),
+    differenceLayerSize.toLocaleString("en"),
+    percentageChange.toLocaleString("en", pctFormat),
+  ];
+
+  layerSizeChangeRows.push(row);
 }
 
-let layerSizeChange = `
+const layerSizeChangeTable = [
+  ...diffHeaderRow,
+  ...layerSizeChangeRows.map((row) => row.join(" | ")),
+].join("\n");
+
+const layerSizeChange = `
 
 ## Layer size comparison
 
-|           | main          | this PR      | change          | % change        |
-|-----------|--------------:|-------------:|----------------:|----------------:|
+${layerSizeChangeTable}
 `;
 
-for (const layer in difference.layerGroup) {
-  layerSizeChange += `${layer} | ${stats1.layerGroup[layer].size.toLocaleString(
-    "en"
-  )} | ${stats2.layerGroup[layer].size.toLocaleString(
-    "en"
-  )} | ${difference.layerGroup[layer].size.toLocaleString("en")} | ${(
-    difference.layerGroup[layer].size / stats1.layerGroup[layer].size
-  ).toLocaleString(undefined, pctFormat)}
-`;
-}
-
-console.log(summaryChange);
-console.log(layerCountChange);
 console.log(layerSizeChange);
