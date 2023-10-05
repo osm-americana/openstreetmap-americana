0bae50b4ec2caccc02aca964e4a6a982f373516b
Clay Smalley <claysmalley@gmail.com>
2023-10-04 20:10:22 -0400

    add Antonio web font

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -3753,7 +3753,7 @@ export function loadShields() {
       bannerTextHaloColor: Color.backgroundFill,
       bannerHeight: 9,
       bannerPadding: 1,
-      shieldFont: '"sans-serif-condensed", "Arial Narrow", sans-serif',
+      shieldFont: 'Antonio, "sans-serif-condensed", "Arial Narrow", sans-serif',
       shieldSize: 20,
     },
   };
