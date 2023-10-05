175317a5bc2b6bbeb498173d3cac964eeaaa7374
Brian Sperlongano <zelonewolf@gmail.com>
2023-10-04 21:49:45 -0400

    Restore shield deltas

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -509,7 +509,7 @@ export function loadShields() {
       textColor: Color.shields.brown,
       colorLighten: Color.shields.brown,
     },
-    ["XXXX"] //FOR TESTING - RESTORE TO HIST BEFORE COMMITTING PR
+    ["HIST"]
   );
 
   // Federal Agencies
