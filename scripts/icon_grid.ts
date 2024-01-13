b6aa55bcf7858f0922738cf373304f058fec3085
Brian Sperlongano <zelonewolf@gmail.com>
2024-01-12 19:59:40 -0500

    Script for icon gridding

---
+++ scripts/icon_grid.ts
@@ -0,0 +1,69 @@
+import sharp from "sharp";
+import fs from "fs";
+import path from "path";
+
+const svgFilename = process.argv[2];
+const outputFilenameBase = path.parse(svgFilename).name;
+const outputFilename = `${outputFilenameBase}_preview.png`;
+const scale = 40;
+
+// Function to generate a pixel grid
+function generateGridPattern(): Buffer {
+  const gridSvg = `<svg width="${scale}" height="${scale}" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="${scale}" height="${scale}" fill="none"/>
+        <path d="M ${scale} 0 V ${scale} M 0 ${scale} H ${scale}" stroke="magenta" stroke-opacity="0.9" stroke-width="1"/>
+        <path d="M ${scale / 2} 0 V ${scale} M 0 ${
+    scale / 2
+  } H ${scale}" stroke="gray" stroke-opacity="0.2" stroke-width="1"/>
+
+  </svg>`;
+  return Buffer.from(gridSvg);
+}
+
+// Convert and scale SVG
+async function convertAndScaleSVG(svgFilename: string): Promise<void> {
+  try {
+    const svgBuffer = fs.readFileSync(svgFilename);
+
+    // Get dimensions of the original SVG
+    const metadata = await sharp(svgBuffer).metadata();
+    const width = metadata.width! * scale;
+    const height = metadata.height! * scale;
+
+    // Resize the SVG
+    const resizedSvgBuffer = await sharp(svgBuffer, {
+      density: 72 * scale,
+    })
+      .resize(width, height)
+      .toBuffer();
+
+    // Generate a pixel grid pattern
+    const gridPattern = generateGridPattern();
+
+    // Composite the scaled image over the grid
+    sharp({
+      create: {
+        width: width,
+        height: height,
+        channels: 4,
+        background: { r: 255, g: 255, b: 255, alpha: 0 },
+      },
+    })
+      .composite([
+        { input: resizedSvgBuffer, blend: "over" },
+        { input: generateGridPattern(), tile: true, blend: "over" },
+      ])
+      .toFile(outputFilename)
+      .then(() => console.log(`Wrote ${outputFilename}`))
+      .catch((err) => console.error(err));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+if (!svgFilename) {
+  console.error("Please provide an SVG filename.");
+  process.exit(1);
+}
+
+convertAndScaleSVG(svgFilename);
