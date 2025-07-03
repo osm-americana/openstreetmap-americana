96821a4f77201588d0f9061392f14249ae0a0f36
Brian Sperlongano <zelonewolf@gmail.com>
2025-07-02 21:41:57 -0400

    Refactor out icon definitions

--- src/layer/poi.js
+++ src/layer/poi.js
@@ -1,294 +1,335 @@
 import * as label from "../constants/label.js";
 import * as Color from "../constants/color.js";
 
+//Define which tile server attributes are associated with an icon.
+var iconAttrs = {
+  aerialway_station: {
+    aerialway: ["station"],
+  },
+  bar: {
+    bar: ["bar"],
+    beer: ["beer", "pub"],
+  },
+  bookstore: {
+    library: ["books"],
+  },
+  bus_station: {
+    bus: ["bus_station"],
+  },
+  bus_stop: {
+    bus: ["bus_stop"],
+  },
+  car_repair: {
+    car: ["car_repair"],
+  },
+  car_shop: {
+    car: ["car"],
+  },
+  charging_station: {
+    fuel: ["charging_station"],
+  },
+  taxi: {
+    office: ["taxi"],
+  },
+  coffee: {
+    cafe: ["cafe"],
+  },
+  fuel: {
+    fuel: ["fuel"],
+  },
+  fire_station: {
+    fire_station: ["fire_station"],
+  },
+  hospital: {
+    hospital: ["hospital"],
+  },
+  hotel: {
+    lodging: ["hotel", "motel", "guest_house"],
+  },
+  hostel: {
+    lodging: ["hostel"],
+  },
+  library: {
+    library: ["library"],
+  },
+  medical: {
+    hospital: ["clinic"],
+    doctors: ["doctors"],
+  },
+  museum: {
+    museum: ["museum"],
+  },
+  parking: {
+    parking: ["parking"],
+  },
+  police: {
+    police: ["police"],
+  },
+  post_office: {
+    post: ["post_office"],
+  },
+  pow_buddhist: {
+    place_of_worship: ["buddhist"],
+  },
+  pow_christian: {
+    place_of_worship: ["christian"],
+  },
+  pow_hindu: {
+    place_of_worship: ["hindu"],
+  },
+  pow_jewish: {
+    place_of_worship: ["jewish"],
+  },
+  pow_muslim: {
+    place_of_worship: ["muslim"],
+  },
+  pow_sikh: {
+    place_of_worship: ["sikh"],
+  },
+  pow_shinto: {
+    place_of_worship: ["shinto"],
+  },
+  pow_taoist: {
+    place_of_worship: ["taoist"],
+  },
+  railway_station: {
+    railway: ["station", "halt", "subway"],
+  },
+  railway_stop: {
+    railway: ["tram_stop"],
+  },
+  restaurant: {
+    restaurant: ["restaurant"],
+    fast_food: ["fast_food", "food_court"],
+  },
+  school: {
+    school: ["kindergarten", "school"],
+  },
+  supermarket: {
+    grocery: ["supermarket"],
+  },
+  college: {
+    college: ["college", "university"],
+  },
+  townhall: {
+    town_hall: ["townhall"],
+  },
+};
+
+//Define how the icon is displayed.
 var iconDefs = {
   aerialway_station: {
-    classes: {
-      aerialway: ["station"],
-    },
+    classes: iconAttrs.aerialway_station,
     sprite: "poi_aerialway_circle",
     color: Color.poi.transport,
     description: "Aerial lift station",
   },
   bar: {
-    classes: {
-      bar: ["bar"],
-      beer: ["beer", "pub"],
-    },
+    classes: iconAttrs.bar,
     sprite: "poi_martini_glass",
     color: Color.poi.consumer,
     description: "Bar or pub",
   },
   bookstore: {
-    classes: {
-      library: ["books"],
-    },
+    classes: iconAttrs.bookstore,
     sprite: "poi_book_upright",
     color: Color.poi.consumer,
     description: "Bookstore",
   },
   bus_station: {
-    classes: {
-      bus: ["bus_station"],
-    },
+    classes: iconAttrs.bus_station,
     sprite: "poi_bus_circle",
     color: Color.poi.transport,
     description: "Bus station",
   },
   bus_stop: {
-    classes: {
-      bus: ["bus_stop"],
-    },
+    classes: iconAttrs.bus_stop,
     sprite: "poi_bus",
     color: Color.poi.transport,
     description: "Bus stop",
   },
   car_repair: {
-    classes: {
-      car: ["car_repair"],
-    },
+    classes: iconAttrs.car_repair,
     sprite: "poi_car_repair",
     color: Color.poi.consumer,
     description: "Car mechanic",
   },
   car_shop: {
-    classes: {
-      car: ["car"],
-    },
+    classes: iconAttrs.car_shop,
     sprite: "poi_car_shop",
     color: Color.poi.consumer,
     description: "Car dealership",
   },
   charging_station: {
-    classes: {
-      fuel: ["charging_station"],
-    },
+    classes: iconAttrs.charging_station,
     sprite: "poi_charging_station",
     color: Color.poi.consumer,
     description: "Charging station",
   },
   taxi: {
-    classes: {
-      office: ["taxi"],
-    },
+    classes: iconAttrs.taxi,
     sprite: "poi_taxi",
     color: Color.poi.transport,
     description: "Taxi stand",
   },
   coffee: {
-    classes: {
-      cafe: ["cafe"],
-    },
+    classes: iconAttrs.coffee,
     sprite: "poi_coffee_cup",
     color: Color.poi.consumer,
     description: "Coffee shop",
   },
   fuel: {
-    classes: {
-      fuel: ["fuel"],
-    },
+    classes: iconAttrs.fuel,
     sprite: "poi_fuel",
     color: Color.poi.consumer,
     description: "Gas station",
   },
   fire_station: {
-    classes: {
-      fire_station: ["fire_station"],
-    },
+    classes: iconAttrs.fire_station,
     sprite: "poi_fire_station",
     color: Color.poi.infrastructure,
     description: "Fire station",
   },
   hospital: {
-    classes: {
-      hospital: ["hospital"],
-    },
+    classes: iconAttrs.hospital,
     sprite: "poi_hospital",
     color: Color.poi.infrastructure,
     description: "Hospital",
   },
   hotel: {
-    classes: {
-      lodging: ["hotel", "motel", "guest_house"],
-    },
+    classes: iconAttrs.hotel,
     sprite: "poi_hotel",
     color: Color.poi.consumer,
     description: "Hotel",
   },
   hostel: {
-    classes: {
-      lodging: ["hostel"],
-    },
+    classes: iconAttrs.hostel,
     sprite: "poi_hostel",
     color: Color.poi.consumer,
     description: "Hostel",
   },
   library: {
-    classes: {
-      library: ["library"],
-    },
+    classes: iconAttrs.library,
     sprite: "poi_book_upright",
     color: Color.poi.infrastructure,
     description: "Library",
   },
   medical: {
-    classes: {
-      hospital: ["clinic"],
-      doctors: ["doctors"],
-    },
+    classes: iconAttrs.medical,
     sprite: "poi_health_cross",
     color: Color.poi.infrastructure,
     description: "Doctor's office or clinic",
   },
   museum: {
-    classes: {
-      museum: ["museum"],
-    },
+    classes: iconAttrs.museum,
     sprite: "poi_museum",
     color: Color.poi.attraction,
     description: "Museum",
   },
   parking: {
-    classes: {
-      parking: ["parking"],
-    },
+    classes: iconAttrs.parking,
     sprite: "poi_p",
     color: Color.poi.infrastructure,
     description: "Parking",
   },
   police: {
-    classes: {
-      police: ["police"],
-    },
+    classes: iconAttrs.police,
     sprite: "poi_police_shield",
     color: Color.poi.infrastructure,
     description: "Police station",
   },
   post_office: {
-    classes: {
-      post: ["post_office"],
-    },
+    classes: iconAttrs.post_office,
     sprite: "poi_envelope",
     color: Color.poi.infrastructure,
     description: "Post office",
   },
   pow_buddhist: {
-    classes: {
-      place_of_worship: ["buddhist"],
-    },
+    classes: iconAttrs.pow_buddhist,
     sprite: "poi_pow_buddhist",
     color: Color.poi.infrastructure,
     description: "Buddhist place of worship",
   },
   pow_christian: {
-    classes: {
-      place_of_worship: ["christian"],
-    },
+    classes: iconAttrs.pow_christian,
     sprite: "poi_pow_christian",
     color: Color.poi.infrastructure,
     description: "Christian place of worship",
   },
   pow_hindu: {
-    classes: {
-      place_of_worship: ["hindu"],
-    },
+    classes: iconAttrs.pow_hindu,
     sprite: "poi_pow_hindu",
     color: Color.poi.infrastructure,
     description: "Hindu place of worship",
   },
   pow_jewish: {
-    classes: {
-      place_of_worship: ["jewish"],
-    },
+    classes: iconAttrs.pow_jewish,
     sprite: "poi_pow_jewish",
     color: Color.poi.infrastructure,
     description: "Jewish place of worship",
   },
   pow_muslim: {
-    classes: {
-      place_of_worship: ["muslim"],
-    },
+    classes: iconAttrs.pow_muslim,
     sprite: "poi_pow_muslim",
     color: Color.poi.infrastructure,
     description: "Muslim place of worship",
   },
   pow_sikh: {
-    classes: {
-      place_of_worship: ["sikh"],
-    },
+    classes: iconAttrs.pow_sikh,
     sprite: "poi_pow_sikh",
     color: Color.poi.infrastructure,
     description: "Sikh place of worship",
   },
   pow_shinto: {
-    classes: {
-      place_of_worship: ["shinto"],
-    },
+    classes: iconAttrs.pow_shinto,
     sprite: "poi_pow_shinto",
     color: Color.poi.infrastructure,
     description: "Shinto place of worship",
   },
   pow_taoist: {
-    classes: {
-      place_of_worship: ["taoist"],
-    },
+    classes: iconAttrs.pow_taoist,
     sprite: "poi_pow_taoist",
     color: Color.poi.infrastructure,
     description: "Taoist place of worship",
   },
   railway_station: {
-    classes: {
-      railway: ["station", "halt", "subway"],
-    },
+    classes: iconAttrs.railway_station,
     sprite: "poi_rail_circle",
     color: Color.poi.transport,
     description: "Train or subway station",
   },
   railway_stop: {
-    classes: {
-      railway: ["tram_stop"],
-    },
+    classes: iconAttrs.railway_stop,
     sprite: "poi_rail",
     color: Color.poi.transport,
     description: "Tram stop",
   },
   restaurant: {
-    classes: {
-      restaurant: ["restaurant"],
-      fast_food: ["fast_food", "food_court"],
-    },
+    classes: iconAttrs.restaurant,
     sprite: "poi_restaurant",
     color: Color.poi.consumer,
     description: "Restaurant or fast food",
   },
   school: {
-    classes: {
-      school: ["kindergarten", "school"],
-    },
+    classes: iconAttrs.school,
     sprite: "poi_school",
     color: Color.poi.infrastructure,
     description: "School",
   },
   supermarket: {
-    classes: {
-      grocery: ["supermarket"],
-    },
+    classes: iconAttrs.supermarket,
     sprite: "poi_supermarket",
     color: Color.poi.consumer,
     description: "Supermarket",
   },
   college: {
-    classes: {
-      college: ["college", "university"],
-    },
+    classes: iconAttrs.college,
     sprite: "poi_mortarboard",
     color: Color.poi.infrastructure,
     description: "College or university",
   },
   townhall: {
-    classes: {
-      town_hall: ["townhall"],
-    },
+    classes: iconAttrs.townhall,
     sprite: "poi_town_hall",
     color: Color.poi.infrastructure,
     description: "City hall",
