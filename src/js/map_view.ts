e96d253b26534d18fb51d168fd3c57320ddafb2d
Minh Nguyễn <mxn@1ec5.org>
2026-01-30 01:02:35 -0800

    Added time slider

--- src/js/map_view.ts
+++ src/js/map_view.ts
@@ -1,4 +1,5 @@
 import { getLocales, localizeStyle } from "@americana/diplomat";
+import { filterByDate } from "@openhistoricalmap/maplibre-gl-dates";
 import maplibregl from "maplibre-gl";
 import { hillshading } from "../layer/hillshade.js";
 
@@ -28,6 +29,19 @@ export class MapView extends maplibregl.Map {
         newValue ? "visible" : "none"
       );
       this.fire("americana.terrain");
+    });
+  }
+
+  get date(): Date | null {
+    return this.getGlobalState().date;
+  }
+
+  set date(newValue: Date) {
+    console.log(`Setting date to ${newValue}`);
+    this.setGlobalStateProperty("date", newValue);
+    Promise.resolve(this.style.loaded() || this.once("styledata")).then(() => {
+      filterByDate(this, newValue);
+      this.fire("americana.datechanged");
     });
   }
 }
