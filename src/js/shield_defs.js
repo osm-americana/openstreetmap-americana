56a69dc01afc8a702688b0b541d30b81d842de07
Minh Nguyễn <mxn@1ec5.org>
2026-01-31 19:18:05 -0800

    Added German federal motorway shield

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -4123,6 +4123,15 @@ export function loadShields() {
   });
 
   // Germany
+  shields["DE:BAB"] = hexagonVerticalShield(
+    3,
+    Color.shields.blue,
+    Color.shields.white,
+    Color.shields.white,
+    0,
+    34
+  );
+
   shields["DE:national"] = roundedRectShield(
     Color.shields.yellow,
     Color.shields.black,
