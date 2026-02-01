dcacc33a086c98c96b85ed49b43b26c6e2e8a886
Minh Nguyễn <mxn@1ec5.org>
2026-01-31 19:47:29 -0800

    Polish A and S roads

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -4474,7 +4474,7 @@ export function loadShields() {
   );
 
   // Poland
-  shields["PL:expressway"] = shields["PL:motorway"] = roundedRectShield(
+  shields["PL:A"] = shields["PL:S"] = roundedRectShield(
     Color.shields.red,
     Color.shields.white,
     Color.shields.white,
