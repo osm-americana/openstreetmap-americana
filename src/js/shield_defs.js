c0c3e3e2012a534db5e96bc5ae20cb569932d5ea
Minh Nguyễn <mxn@1ec5.org>
2026-02-24 10:56:40 -0800

    Added Danish, German, Polish, Swiss shields
    https://wiki.openstreetmap.org/wiki/Special:PermanentLink/2951531

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -4028,6 +4028,14 @@ export function loadShields() {
   );
 
   // Switzerland
+  shields["CH:A"] = hexagonVerticalShield(
+    3,
+    Color.shields.red,
+    Color.shields.white,
+    Color.shields.white,
+    0,
+    34
+  );
   shields["ch:national"] = roundedRectShield(
     Color.shields.blue,
     Color.shields.white
@@ -4046,6 +4054,23 @@ export function loadShields() {
     Color.shields.white,
     Color.shields.white,
     34
+  );
+
+  // Denmark
+  shields["DK:E"] = roundedRectShield(
+    Color.shields.green,
+    Color.shields.white,
+    Color.shields.white
+  );
+  shields["DK:P"] = roundedRectShield(
+    Color.shields.yellow,
+    Color.shields.black,
+    Color.shields.black
+  );
+  shields["DK:A"] = shields["DK:S"] = roundedRectShield(
+    Color.shields.white,
+    Color.shields.black,
+    Color.shields.black
   );
 
   // Estonia
@@ -4142,7 +4167,7 @@ export function loadShields() {
     );
   });
 
-  // Germany
+  // Germany, West Germany
   shields["DE:BAB"] = hexagonVerticalShield(
     3,
     Color.shields.blue,
@@ -4152,7 +4177,15 @@ export function loadShields() {
     34
   );
 
-  shields["DE:national"] = roundedRectShield(
+  shields["DE:B"] = shields["DE:R"] = roundedRectShield(
+    Color.shields.yellow,
+    Color.shields.black,
+    Color.shields.black,
+    34
+  );
+
+  // East Germany
+  shields["DD:F"] = roundedRectShield(
     Color.shields.yellow,
     Color.shields.black,
     Color.shields.black,
@@ -4345,10 +4378,18 @@ export function loadShields() {
     Color.shields.white,
     34
   );
-  shields["pl:national"] = roundedRectShield(
-    Color.shields.red,
+  shields["PL:DK"] = roundedRectShield(Color.shields.red, Color.shields.white);
+  shields["PL:DW"] = roundedRectShield(
+    Color.shields.yellow,
+    Color.shields.black,
+    Color.shields.black
+  );
+  shields["PL:DM"] = roundedRectShield(
+    Color.shields.green,
+    Color.shields.white,
     Color.shields.white
   );
+  shields["PL:DP"] = roundedRectShield(Color.shields.red, Color.shields.white);
 
   // Portugal
   shields["PT:national"] = roundedRectShield(
